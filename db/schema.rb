ab0ec7a39969b01fd579788571daf1ea1c302d46
matthiashaefeli <haefeli.matthias2gmail.com>
2021-06-06 16:39:13 -0500

    add list for blog list etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_204606) do
+ActiveRecord::Schema.define(version: 2021_06_06_212734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,13 @@ ActiveRecord::Schema.define(version: 2021_04_12_204606) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["commenteable_type", "commenteable_id"], name: "index_comments_on_commenteable"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "notes", force: :cascade do |t|
